21101953a37b69e7f9e17ea558e1df3c95261d14
inevitableconsequences54 <161323477+inevitableconsequences@users.noreply.github.com>
2025-01-08 19:15:18 +0500

    Returned parf of functions from old mod inspector

--- ModInspector/ModInspector/ModsPage.xaml.cs
+++ ModInspector/ModInspector/ModsPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using System.Linq;
 
 namespace ModInspector
 {
@@ -7,9 +11,60 @@ namespace ModInspector
     /// </summary>
     public partial class ModsPage : Page
     {
-        public ModsPage()
+        OpenFolderDialog openFolderDialog = new();
+        public string? ModsPath { get; set; } = null;
+
+        public ModsPage() => InitializeComponent();
+        private void btnSelectAll_Click(object sender, RoutedEventArgs e) => modsListBox.SelectAll();
+        private void btnRefreshListOfMods_Click(object sender, RoutedEventArgs e) => Refresh();
+
+        private void btnSwitchState_Click(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            foreach (string item in modsListBox.SelectedItems.Cast<string>().ToList())
+            {
+                if (Path.GetExtension(item) == ".deactivated")
+                    ChangeFileExtension(item, ".jar");
+                else if (Path.GetExtension(item) == ".jar")
+                    ChangeFileExtension(item, ".deactivated");
+            }
+            Refresh();
+        }
+
+        private void ChangeFileExtension(string fileNameWithoutExtension, string newExtension)
+        {
+            string oldFilePath = Path.Combine(ModsPath, fileNameWithoutExtension);
+            string newFilePathWithExtension = Path.ChangeExtension(oldFilePath, newExtension.TrimStart('.'));
+
+            if (File.Exists(oldFilePath))
+            {
+                try
+                {
+                    File.Move(oldFilePath, newFilePathWithExtension);
+                    Refresh();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Maybe something is wrong?: {ex.Message}");
+                }
+            }
+            else MessageBox.Show($"File {oldFilePath} not found.");
+        }
+
+        private void btnSelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFolderDialog.ShowDialog() == true)
+                ModsPath = openFolderDialog.FolderName;
+            Refresh();
+        }
+
+        void Refresh()
+        {
+            if (!string.IsNullOrEmpty(ModsPath))
+            {
+                modsListBox.Items.Clear();
+                foreach (string item in Directory.GetFiles(ModsPath))
+                    modsListBox.Items.Add(item.Substring(item.LastIndexOf('\\') + 1));
+            }
         }
     }
 }
